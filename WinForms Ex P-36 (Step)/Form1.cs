5fb2453f7adcb8c69a70c58e80397c7b2b4b508d
Tetyana-K <tanya.kyryk@gmail.com>
2025-09-01 16:23:05 +0300

    Add project files.

---
+++ WinForms Ex P-36 (Step)/Form1.cs
@@ -0,0 +1,20 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WinForms_Ex_P_36__Step_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<string> pictures =
+        [
+            @"https://hips.hearstapps.com/hmg-prod/images/gettyimages-180680638-676f621f720bc.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
+            @"https://www.cdc.gov/healthy-pets/media/images/2024/04/GettyImages-598175960-cute-dog-headshot.jpg",
+            @"https://blogscdn.thehut.net/app/uploads/sites/2405/2025/02/JWB-5-Image-1_1738922557.jpg"
+        ];
+    }
+}
